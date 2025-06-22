6d247653132771ffa1b1fe784c149f096b7333e1
ThembaMtshelwane <thembamm3@gmail.com>
2025-06-22 19:02:58 +0200

    View Tasks UI: Fetching data from the api

--- view/src/pages/ViewTasks.tsx
+++ view/src/pages/ViewTasks.tsx
@@ -1,20 +1,34 @@
 import TaskCard from "../components/TaskCard";
+import { useGetTasksQuery } from "../slices/taskSlice";
 
 const ViewTasks = () => {
+  const { data, isFetching } = useGetTasksQuery();
+
+  const totalDuration =
+    data?.data.reduce((sum, task) => sum + task.duration, 0) || 0;
+
   return (
     <section className="flex flex-col gap-4">
       <div className="text-center text-blue-700 space-y-2">
         <h1 className="text-5xl font-semibold">All Tasks </h1>
         <div className="flex flex-col">
           <p>Total Time Worked:</p>
-          <span className="text-3xl font-extrabold">{18.23}hrs</span>
+          <span className="text-3xl font-extrabold">
+            {totalDuration.toFixed(2)}hrs
+          </span>
         </div>
       </div>
 
       <div className=" flex flex-col w-full sm:w-[70%] mx-auto justify-center gap-4">
-        <TaskCard name={"Task 1"} duration={0} />
-        <TaskCard name={"Task 1"} duration={1.75} />
-        <TaskCard name={"Task 1"} duration={0.25} />
+        {isFetching ? (
+          <>Loading...</>
+        ) : (
+          <>
+            {data?.data.map((task) => (
+              <TaskCard name={task.name} duration={task.duration} />
+            ))}
+          </>
+        )}
       </div>
     </section>
   );
