ddc2f0d32bde129ad05d82acff9df6001e3c6fe8
ThembaMtshelwane <thembamm3@gmail.com>
2025-06-22 19:25:27 +0200

    Completed the create task controller

--- server/src/controller/tasks.controller.ts
+++ server/src/controller/tasks.controller.ts
@@ -1,12 +1,28 @@
 import expressAsyncHandler from "express-async-handler";
 import { Request, Response } from "express";
+import supabase from "../db/supabase.config";
+import { SUPABASE_TABLE_NAME } from "../constants/env.constants";
 
 // Controller functions for task management
 // These functions handle the creation, retrieval, updating, and deletion of tasks.
 
 // Create a new task
 export const createTask = expressAsyncHandler(
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { error } = await supabase.from(SUPABASE_TABLE_NAME).insert(req.body);
+
+    if (error) {
+      res.status(500).json({
+        message: "Error creating task",
+        error: error.message,
+      });
+      return;
+    }
+
+    res.status(201).json({
+      message: "Task created successfully",
+    });
+  }
 );
 
 // Retrieve all tasks
